9c03ab3a53f853dea01b0f11be4cf596406e0a53
itxAdie <itxadiert@gmail.com>
2024-08-23 16:56:18 +0500

    INIT

---
+++ Attendance system/attendance_system_admin/android/app/src/main/kotlin/com/example/attendance_system_admin/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.attendance_system_admin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ Attendance system/attendance_system_user/android/app/src/main/kotlin/com/example/attendance_system_user/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.attendance_system_user
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
